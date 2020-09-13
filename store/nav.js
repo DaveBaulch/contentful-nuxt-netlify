21d34567d6e8c9a0fe2097c7d12ba37d02c8fa05
Dave Baulch <davebaulch@yahoo.co.uk>
2020-09-13 09:55:55 +0100

    code tidied

--- store/nav.js
+++ store/nav.js
@@ -19,21 +19,5 @@ export const actions = {
 export const mutations = {
   TOGGLE_SIDEBAR (state) {
     state.sidebarActive = !state.sidebarActive
-
-    // const navButton = document.getElementById('nav-button')
-    // const sidebarElement = document.getElementById('sidebar')
-
-    // const navButton = this.$refs.navButton
-    // const sidebarElement = this.$refs.sidebar
-
-    // if (navButton.getAttribute('aria-expanded') === 'false') {
-    //   navButton.setAttribute('aria-expanded', 'true')
-    //   sidebarElement.setAttribute('aria-expanded', 'true')
-    //   sidebarElement.setAttribute('aria-hidden', 'false')
-    // } else {
-    //   navButton.setAttribute('aria-expanded', 'false')
-    //   sidebarElement.setAttribute('aria-expanded', 'false')
-    //   sidebarElement.setAttribute('aria-hidden', 'true')
-    // }
   }
 }
