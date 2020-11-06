913927db97d96b1db69d26d78ffe02214a42b849
Dave Baulch <davebaulch@yahoo.co.uk>
2020-11-06 12:12:17 +0000

    SASS tidied

---
+++ .history/components/global/AppNavTransition_20201106120934.vue
@@ -0,0 +1,217 @@
+<template>
+  <transition-group tag="div">
+    <img key="photo1" src="~/assets/layout/dave_250.jpg" class="photo" alt="Dave Baulch">
+
+    <div key="button-name" class="button-name">
+      <OpenContactButton text="Contact me!" />
+    </div>
+
+    <h2 key="profile-name" class="profile-name">
+      <span class="profile-name__name">Dave Baulch</span>
+      <span class="profile-name__title">Front-End Developer</span>
+    </h2>
+  </transition-group>
+</template>
+
+<script>
+
+export default {
+}
+</script>
+
+<style lang="scss" scoped>
+@import '~assets/sass/utilities/_mixins.scss';
+
+  .photo {
+    width: 120px;
+    height: 134px;
+    position: absolute;
+    left: 20px;
+    top: 115px;
+    transition: all 0.5s ease-in-out;
+    border: 2px solid $white;
+    border-radius: 5px;
+
+    .about &,
+    .cv &,
+    .work &,
+    .blog &,
+    .blog-slug & {
+      width: 90px;
+      height: 100px;
+      transform: translate3d(0, -36px, 0);
+    }
+
+    @include breakpoint(sm) {
+      width: 151px;
+      height: 168px;
+      left: 20px;
+      top: 87px;
+      transform: translate3d(0, 0, 0);
+
+      .about &,
+      .cv &,
+      .work &,
+      .blog &,
+      .blog-slug & {
+        width: 90px;
+        height: 100px;
+        transform: translate3d(0, -4px, 0);
+      }
+    }
+
+    @include breakpoint(md) {
+      width: 252px;
+      height: 281px;
+      left: 30px;
+      top: 120px;
+      transform: translate3d(0, 0, 0);
+
+      .about &,
+      .cv &,
+      .work &,
+      .blog &,
+      .blog-slug &{
+        width: 179px;
+        height: 200px;
+        transform: translate3d(0, -37px, 0)
+      }
+    }
+  }
+
+  .button-name {
+    position: absolute;
+    //left: 143px;
+    left: 153px;
+    top: 138px;
+    transition: all 0.5s ease-in-out;
+
+    .about &,
+    .cv &,
+    .work &,
+    .blog &,
+    .blog-slug & {
+      display: none;
+      transform: translate3d(-30px, -10px, 0);
+    }
+
+    @include breakpoint(sm) {
+      left: 190px;
+      top: 130px;
+      display: block;
+
+      .about &,
+      .cv &,
+      .work &,
+      .blog &,
+      .blog-slug & {
+        display: block;
+        transform: translate3d(-70px, 4px, 0);
+      }
+    }
+
+    @include breakpoint(md) {
+      left: 300px;
+      top: 224px;
+
+      .about &,
+      .cv &,
+      .work &,
+      .blog &,
+      .blog-slug & {
+        transform: translate3d(-70px, 9px, 0);
+      }
+    }
+  }
+
+  .profile-name {
+    position: absolute;
+    //left: 140px;
+    left: 150px;
+    top: 180px;
+    font-size: 2.6rem;
+    transition: all 0.5s ease-in-out;
+    color: #0a3246;
+
+    @include breakpoint(sm) {
+      left: 150px;
+    }
+
+    .profile-name__name {
+      font-weight: bold;
+      display: block;
+      line-height: 1.3;
+
+      @include breakpoint(md) {
+        line-height: 1.5;
+      }
+    }
+
+    .profile-name__title {
+      text-transform: uppercase;
+      display: block;
+      font-size: 1.6rem;
+      line-height: 1;
+
+      @include breakpoint(md) {
+        font-size: 2.6rem;
+      }
+
+      .about &,
+      .cv &,
+      .work &,
+      .blog &,
+      .blog-slug & {
+        display: none;
+      }
+    }
+
+    .about &,
+    .cv &,
+    .work &,
+    .blog &,
+    .blog-slug & {
+      //left: 180px;
+      left: 190px;
+      top: 200px;
+      transform: translate3d(-71px, -130px, 0);
+      color: $white;
+      font-weight: normal;
+    }
+
+    @include breakpoint(sm) {
+      left: 190px;
+      top: 185px;
+      transform: translate3d(0, 0, 0);
+
+      .about &,
+      .cv &,
+      .work &,
+      .blog &,
+      .blog-slug & {
+        left: 190px;
+        top: 200px;
+      }
+    }
+
+    @include breakpoint(md) {
+      left: 300px;
+      top: 280px;
+      font-size: 5rem;
+      transform: translate3d(0, 0, 0);
+
+      .about &,
+      .cv &,
+      .work &,
+      .blog &,
+      .blog-slug & {
+        transform: translate3d(-71px, -130px, 0);
+        color: $white;
+        font-size: 4rem;
+        left: 300px;
+        top: 280px;
+      }
+    }
+  }
+
+</style>
---
+++ .history/components/global/AppSidebar_20201106120814.vue
@@ -0,0 +1,121 @@
+<template>
+  <div class="sidebar-wrapper">
+    <div v-if="showSidebar" class="sidebar-background" @click="toggleSidebar" />
+
+    <div
+      id="sidebar"
+      ref="sidebar"
+      class="sidebar"
+      :class="{ active: showSidebar }"
+      aria-label="Primary Navigation"
+      aria-expanded="false"
+      aria-hidden="true"
+    >
+      <CloseSidebarButton class="js-sidebar-focussable" />
+
+      <div class="sidebar-inner">
+        <AppMobileLinks />
+        <OpenContactButton2
+          text="Contact me!"
+          class="js-sidebar-focussable"
+          tabindex="-1"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    showSidebar() {
+      return this.$store.getters["nav/showSidebar"];
+    }
+  },
+  watch: {
+    showSidebar(val) {
+      const sidebarElement = this.$refs.sidebar;
+      if (sidebarElement.getAttribute("aria-expanded") === "false") {
+        sidebarElement.setAttribute("aria-expanded", "true");
+        sidebarElement.setAttribute("aria-hidden", "false");
+      } else {
+        sidebarElement.setAttribute("aria-expanded", "false");
+        sidebarElement.setAttribute("aria-hidden", "true");
+      }
+    }
+  },
+  methods: {
+    toggleSidebar() {
+      this.sidebarActive = !this.sidebarActive;
+      this.$store.dispatch("nav/toggleSidebar");
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "~assets/sass/utilities/_mixins.scss";
+
+.sidebar-wrapper {
+  overflow-y: auto;
+
+  @include breakpoint(md) {
+    display: none;
+  }
+}
+
+.sidebar {
+  height: 100%;
+  width: 100%;
+  z-index: 10000;
+  position: fixed;
+  top: 0;
+  left: 0;
+  box-sizing: border-box;
+  padding: 20px;
+  transform: translateX(-100%);
+  transition: all 0.5s ease-out;
+  background: rgba(10, 50, 70, 0.5);
+
+  @include breakpoint(sm) {
+    width: 90%;
+    padding: 25px;
+  }
+
+  &.active {
+    transform: translateX(0);
+    background: rgba(10, 50, 70, 1);
+  }
+}
+
+.sidebar-background {
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.8);
+  z-index: 1000;
+  position: fixed;
+  top: 0;
+  left: 0;
+}
+
+.sidebar-inner {
+  margin-top: 60px;
+  border: 1px solid $white;
+  padding: 3rem 3rem 6rem 3rem;
+  opacity: 0;
+  transition: all 0.2s ease-in-out;
+  border-radius: 1px;
+
+  button {
+    margin: 0 auto;
+    display: block;
+    background-color: rgba(217, 74, 25, 0.9);
+  }
+}
+
+.sidebar.active .sidebar-inner {
+  opacity: 1;
+  transition: all 0.4s ease-in-out;
+  transition-delay: 0.2s;
+}
+</style>
